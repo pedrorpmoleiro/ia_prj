f380a9c4ef8ea663c0a00cbc39e6d9d2c0823e16
Thainá <thainabbraz@gmail.com>
2019-05-16 19:33:45 +0100

    setCellCatch, heuristic v2

--- src/catchBox/HeuristicCatch.java
+++ src/catchBox/HeuristicCatch.java
@@ -6,12 +6,12 @@ public class HeuristicCatch extends Heuristic<CatchProblemSearch, CatchState> {
 
     @Override
     public double compute(CatchState state) {
-        return state.getNumBox();
+        return state.computeDistance(problem.getGoalPosition());
 
     }
 
     @Override
     public String toString() {
-       return "quantity of boxes";
+       return "calculation of distance";
     }
 }
