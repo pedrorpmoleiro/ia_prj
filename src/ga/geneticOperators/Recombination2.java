da1cf2e07f8fc1f1021f1987cdd0100518d5f43e
Pedro Moleiro <pedro.r.p.moleiro@gmail.com>
2019-05-26 15:06:15 +0100

    Algoritmos de Recombinação

--- src/ga/geneticOperators/Recombination2.java
+++ src/ga/geneticOperators/Recombination2.java
@@ -1,25 +1,32 @@
 package ga.geneticOperators;
 
+import ga.GeneticAlgorithm;
 import ga.IntVectorIndividual;
 import ga.Problem;
 
 public class Recombination2<I extends IntVectorIndividual, P extends Problem<I>> extends Recombination<I, P> {
-
-    //TODO this class might require the definition of additional methods and/or attributes
-
     public Recombination2(double probability) {
         super(probability);
     }
 
     @Override
     public void recombine(I ind1, I ind2) {
-        //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        int cut1 = GeneticAlgorithm.random.nextInt(ind1.getNumGenes());
+        int cut2 = GeneticAlgorithm.random.nextInt(ind2.getNumGenes());
+
+        if (cut1 > cut2) {
+            int aux = cut1;
+            cut1 = cut2;
+            cut2 = aux;
+        }
+
+        for (int i = cut1; i < cut2; i++) {
+            ind1.swapGenes(ind2, i);
+        }
     }
 
     @Override
     public String toString(){
-        //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+        return "Two Cut";
     }    
 }
