773e7037f58545e8e3a17bc5808744d433e5f3a4
Thainá <thainabbraz@gmail.com>
2019-06-04 17:46:36 +0100

    Heuristic Class done

--- src/catchBox/HeuristicCatch.java
+++ src/catchBox/HeuristicCatch.java
@@ -6,13 +6,11 @@ public class HeuristicCatch extends Heuristic<CatchProblemSearch, CatchState> {
 
     @Override
     public double compute(CatchState state) {
-        //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+       return state.computeDistance(problem.getGoalPosition());
     }
 
     @Override
     public String toString() {
-        //TODO
-        throw new UnsupportedOperationException("Not Implemented Yet");
+       return "Calculation of distance";
     }
 }
